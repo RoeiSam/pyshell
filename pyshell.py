8a07c729bba0e9204234def6e6f85a570a30294e
Roei Samuel <roei.sam.2005@gmail.com>
2025-07-01 15:00:54 +0300

    Implement event designators

--- pyshell.py
+++ pyshell.py
@@ -4,10 +4,36 @@ Author: Roei Samuel
 Date: 01.07.25
 """
 from pyshell_commands import ls, cd, pwd, echo, logname, man
+from typing import List
 
 COMMAND_NAME = 0
 FIRST_ARGUMENT = 1
 LAST_COMMAND = -1
+
+def convert_command(command: str, commands_history: List[str]) -> str:
+    """
+    Convert input from user to command and arguments.
+    :param command: The input from the user.
+    :param commands_history: History of all commands so far.
+    :return: The command seperated to command name and arguments.
+    """
+    command = command.split()
+    if command[0] == "!!":
+        del command[0]
+        for i in range(len(commands_history[LAST_COMMAND].split())):
+            command.insert(i, commands_history[LAST_COMMAND].split()[i])
+        print(" ".join(command))
+    elif command[0].startswith("!") and command[0][1].isdigit():
+        for i in range(len(commands_history[int(command[0][1]) - 1].split())):
+            command.insert(i + 1, commands_history[int(command[0][1]) - 1].split()[i])
+        del command[0]
+        print(" ".join(command))
+    elif command[0].startswith("!-") and command[0][2].isdigit():
+        for i in range(len(commands_history[-int(command[COMMAND_NAME][2])].split())):
+            command.insert(i + 1, commands_history[-int(command[COMMAND_NAME][2])].split()[i])
+        del command[0]
+        print(" ".join(command))
+    return command
 
 
 def pyshell() -> None:
@@ -19,8 +45,9 @@ def pyshell() -> None:
     print("Welcome to the Python Shell!")
     while True:
         command = input(f"{logname([])}:{pwd([])} -> ")
-        command = command.split()
         if command:
+            command = convert_command(command, commands_history)
+            commands_history.append(" ".join(command))
             try:
                 return_string = eval(f"{command[COMMAND_NAME]}({command[FIRST_ARGUMENT:]})")
                 if return_string:
